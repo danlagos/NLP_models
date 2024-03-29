f01066ceb51b3a5d10ce402edab845a736039594
Daniel Lagos <lagos.d@gmail.com>
2024-03-29 13:50:41 -0700

    work on self attention class.

--- attention_mechanis.py
+++ attention_mechanis.py
@@ -70,3 +70,53 @@ notes on multi-head attention:
     this output is concatenated and passed through another linear layer.
 """
 
+import torch
+import torch.nn as nn
+
+class SelfAttention(nn.Module):
+    def __init__(self, embed_size, heads):
+        super(SelfAttention, self).__init__()
+        self.embed_size = embed_size
+        self.heads = heads
+        self.head_dim = embed_size // heads
+        
+        assert (
+            self.head_dim * heads == embed_size
+        ), "Embedding size needs to be divisible by heads"
+        
+        self.values = nn.Linear(self.head_dim, self.head_dim, bias=False)
+        self.keys = nn.Linear(self.head_dim, self.head_dim, bias=False)
+        self.queries = nn.Linear(self.head_dim, self.head_dim, bias=False)
+        self.fc_out = nn.Linear(heads * self.head_dim, embed_size) # fully connected layer
+    
+    def forward(self, values, keys, query, mask):
+        N = query.shape[0] # number of samples
+        value_len, key_len, query_len = values.shape[1], keys.shape[1], query.shape[1]
+        
+        # split the embedding into self.heads pieces
+        values = values.reshape(N, value_len, self.heads, self.head_dim) # (N, value_len, heads, head_dim)
+        keys = keys.reshape(N, key_len, self.heads, self.head_dim) # (N, key_len, heads, head_dim)
+        queries = query.reshape(N, query_len, self.heads, self.head_dim) # (N, query_len, heads, head_dim)
+        
+        """
+        values = self.values(values)
+        keys = self.keys(keys)
+        queries = self.queries(queries)
+        """
+        
+        # queries shape: (N, query_len, heads, head_dim)
+        # keys shape: (N, key_len, heads, head_dim)
+        # energy shape: (N, heads, query_len, key_len)
+
+        energy = torch.einsum("nqhd, nkhd -> nhqk", [queries, keys])
+        if mask is not None:
+            energy = energy.masked_fill(mask == 0, float("-1e20"))
+        
+        attention = torch.softmax(energy / (self.embed_size ** (1/2)), dim=3)
+        
+        out = torch.einsum("nhql, nlhd -> nqhd", [attention, values]).reshape(
+            N, query_len, self.heads * self.head_dim
+        )
+        
+        out = self.fc_out(out)
+        return out

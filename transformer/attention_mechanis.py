baa923dc3c79660bdef68723417f838509e979d8
Daniel Lagos <lagos.d@gmail.com>
2024-03-29 16:28:39 -0700

    encoder and decoder complete.

--- transformer/attention_mechanis.py
+++ transformer/attention_mechanis.py
@@ -197,7 +197,7 @@ class Encoder(nn.Module):
         
         return out
 
-# start decoder
+# start decoder block
 class DecoderBlock(nn.Module):
     def __init__(self, embed_size, heads, forward_expansion, dropout, device):
         super(DecoderBlock, self).__init__()
@@ -212,4 +212,43 @@ class DecoderBlock(nn.Module):
         attention = self.attention(x, x, x, trg_mask)
         query = self.dropout(self.norm(attention + x))
         out = self.transformer_block(value, key, query, src_mask)
+        return out
+    
+class Decoder(nn.Module):
+    def __init__(
+        self,
+        trg_vocab_size,
+        embed_size,
+        num_layers,
+        heads,
+        forward_expansion,
+        dropout,
+        device,
+        max_length,
+    ):
+        super(Decoder, self).__init__()
+        self.device = device
+        self.word_embedding = nn.Embedding(trg_vocab_size, embed_size)
+        self.position_embedding = nn.Embedding(max_length, embed_size)
+        
+        self.layers = nn.ModuleList(
+            [
+                DecoderBlock(embed_size, heads, forward_expansion, dropout, device)
+                for _ in range(num_layers)
+            ]
+        )
+        
+        self.fc_out = nn.Linear(embed_size, trg_vocab_size)
+        
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, x, enc_out, src_mask, trg_mask):
+        N, seq_length = x.shape
+        positions = torch.arange(0, seq_length).expand(N, seq_length).to(self.device)
+        x = self.dropout((self.word_embedding(x) + self.position_embedding(positions)))
+        
+        for layer in self.layers:
+            x = layer(x, enc_out, enc_out, src_mask, trg_mask)
+        
+        out = self.fc_out(x) # prediction of what word is next
         return out

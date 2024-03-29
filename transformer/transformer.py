7cd9233542c3e3eae3996a839d327806eb2d396b
Daniel Lagos <lagos.d@gmail.com>
2024-03-29 16:55:23 -0700

    finished code.  Testing.

--- transformer/transformer.py
+++ transformer/transformer.py
@@ -99,12 +99,11 @@ class SelfAttention(nn.Module):
         keys = keys.reshape(N, key_len, self.heads, self.head_dim) # (N, key_len, heads, head_dim)
         queries = query.reshape(N, query_len, self.heads, self.head_dim) # (N, query_len, heads, head_dim)
         
-        """
+        # returns the linear transformation of the input        
         values = self.values(values)
         keys = self.keys(keys)
         queries = self.queries(queries)
-        """
-        
+                
         # queries shape: (N, query_len, heads, head_dim)
         # keys shape: (N, key_len, heads, head_dim)
         # energy shape: (N, heads, query_len, key_len)
@@ -252,3 +251,84 @@ class Decoder(nn.Module):
         
         out = self.fc_out(x) # prediction of what word is next
         return out
+    
+class Transformer(nn.Module):
+    def __init__(
+        self,
+        src_vocab_size,
+        trg_vocab_size,
+        src_pad_idx, # padding index, used for masking
+        trg_pad_idx, # padding index, used for masking
+        embed_size=256, # this might have to be adjusted due to GPU memory constraints
+        num_layers=6,
+        forward_expansion=4,
+        heads=8,
+        dropout=0,
+        device="cuda", # change to "cpu" if you don't have a GPU
+        max_length=100,
+    ):
+        super(Transformer, self).__init__()
+        
+        # define encoder
+        self.encoder = Encoder(
+            src_vocab_size,
+            embed_size,
+            num_layers,
+            heads,
+            device,
+            forward_expansion,
+            dropout,
+            max_length,
+        )
+        
+        # define decoder
+        self.decoder = Decoder(
+            trg_vocab_size,
+            embed_size,
+            num_layers,
+            heads,
+            forward_expansion,
+            dropout,
+            device,
+            max_length,
+        )
+        
+        self.src_pad_idx = src_pad_idx
+        self.trg_pad_idx = trg_pad_idx
+        self.device = device
+        
+    def make_src_mask(self, src):
+        # (N, 1, 1, src_len)
+        src_mask = (src != self.src_pad_idx).unsqueeze(1).unsqueeze(2)
+        return src_mask.to(self.device)
+    
+    def make_trg_mask(self, trg):
+        N, trg_len = trg.shape
+        trg_mask = torch.tril(torch.ones((trg_len, trg_len))).expand(
+            N, 1, trg_len, trg_len
+        )
+        
+        return trg_mask.to(self.device)
+    
+    def forward(self, src, trg):
+        src_mask = self.make_src_mask(src)
+        trg_mask = self.make_trg_mask(trg)
+        enc_src = self.encoder(src, src_mask)
+        out = self.decoder(trg, enc_src, src_mask, trg_mask)
+        return out
+
+# example to see if this runs
+if __name__ == "__main__":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    x = torch.tensor([[1, 5, 6, 4, 3, 9, 5, 2, 0], [1, 8, 7, 3, 4, 5, 6, 7, 2]]).to(device)
+    trg = torch.tensor([[1, 7, 4, 3, 5, 9, 2, 0], [1, 5, 6, 2, 4, 7, 6, 2]]).to(device)
+    
+    src_pad_idx = 0
+    trg_pad_idx = 0
+    src_vocab_size = 10
+    trg_vocab_size = 10
+    model = Transformer(src_vocab_size, trg_vocab_size, src_pad_idx, trg_pad_idx).to(device)
+    
+    out = model(x, trg[:, :-1])
+    print(out.shape) # [N, trg_len - 1, trg_vocab_size]
